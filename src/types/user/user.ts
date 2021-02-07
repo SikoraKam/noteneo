839063b3dd265bbf9cd2c1f406c6f3d4c874faa8
SikoraKam <sikorakam3@gmail.com>
2021-02-08 00:21:27 +0100

    feat: profile update mutation

---
+++ src/types/user/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+  email: string;
+  first_name: string;
+  last_name: string;
+  phone?: string;
+  address?: string;
+}
--- src/types/user/userProfileResponse.ts
+++ src/types/user/userProfileResponse.ts
@@ -1,11 +1,8 @@
-export interface UserProfileResponse {
+import { User } from './user';
+
+export interface UserProfileResponse extends User {
   pk: number;
-  email: string;
-  first_name: string;
-  last_name: string;
-  phone?: string;
   date_joined: string;
-  address?: string;
   is_subscriber: boolean;
   subscription_to?: any;
 }
