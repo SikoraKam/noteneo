c3f141eba64887aa280dcdf69746cfd5871915a7
SikoraKam <sikorakam3@gmail.com>
2021-02-07 20:04:47 +0100

    feat: profile edit

--- src/screens/profile/ProfileScreenStack.tsx
+++ src/screens/profile/ProfileScreenStack.tsx
@@ -6,9 +6,13 @@ import { Text } from 'react-native';
 
 import { BottomTabNavigationParamList } from '../AppScreenStack';
 import { ProfileScreen } from './ProfileScreen';
+import { HeaderAppButton } from '../../components/shared/HeaderAppButton';
+import { ProfileEditRouteParameters } from '../../types/ProfileEditRouteParameters';
+import { ProfileEditScreen } from './ProfileEditScreen';
 
 export type ProfileScreenStackParamList = {
   Profile: undefined;
+  ProfileEdit: ProfileEditRouteParameters;
 };
 
 type ProfileScreenStackProps = MaterialBottomTabScreenProps<
@@ -22,6 +26,12 @@ export const ProfileScreenStack: React.FC<ProfileScreenStackProps> = ({
   navigation,
 }) => {
   const theme = useTheme();
+  // const { logout } = useAuth();
+
+  const onLogout = async () => {
+    // await logout();
+    console.log('WYLOGUJ');
+  };
 
   return (
     <Stack.Navigator screenOptions={theme.tallHeader}>
@@ -30,8 +40,26 @@ export const ProfileScreenStack: React.FC<ProfileScreenStackProps> = ({
         component={ProfileScreen}
         options={({ navigation }) => ({
           title: 'Profil',
-          headerLeft: () => <Text>Edytuj</Text>,
+          headerLeft: () => (
+            <HeaderAppButton
+              onPress={() =>
+                navigation.push('ProfileEdit', {
+                  // avatar: userProfile.data?.versioned_avatar,
+                  avatar: require('../../../assets/versioned_initial_avatar.png'), //TODO
+                })
+              }>
+              Edytuj
+            </HeaderAppButton>
+          ),
+          headerRight: () => (
+            <HeaderAppButton onPress={onLogout}>Wyloguj</HeaderAppButton>
+          ),
         })}
+      />
+      <Stack.Screen
+        name="ProfileEdit"
+        component={ProfileEditScreen}
+        options={{ title: 'Edytuj profil' }}
       />
     </Stack.Navigator>
   );
---
+++ src/types/ProfileEditRouteParameters.tsx
@@ -0,0 +1,3 @@
+export interface ProfileEditRouteParameters {
+  avatar: string;
+}
