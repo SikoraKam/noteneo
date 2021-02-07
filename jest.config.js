50a8cec798edb23378cb204075093cffc1444d44
Michał Bar <logout400@gmail.com>
2021-02-07 15:34:12 +0100

    chore: setup eslint and prettier

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+// jest.config.js
+const { defaults: tsjPreset } = require('ts-jest/presets');
+
+module.exports = {
+  ...tsjPreset,
+  preset: 'jest-expo',
+  transform: {
+    ...tsjPreset.transform,
+    '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+  ],
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
+  // This is the only part which you can keep
+  // from the above linked tutorial's config:
+  cacheDirectory: '.jest/cache',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.setup.js',
+  ],
+};
