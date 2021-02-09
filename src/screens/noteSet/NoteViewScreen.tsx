5e0e4dba6c2f21f356d6ee3292b67d3c0c52aefe
Michał Bar <logout400@gmail.com>
2021-02-10 00:21:57 +0100

    feat: prepare premium

--- src/screens/noteSet/NoteViewScreen.tsx
+++ src/screens/noteSet/NoteViewScreen.tsx
@@ -1,9 +1,12 @@
 import { StackScreenProps } from '@react-navigation/stack';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NoteSetScreenStackParamList } from './NoteSetScreenStack';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { NoteView } from '../../components/note/NoteView';
 import { Container } from '../../components/layout/Container';
+import { useQueryClient } from 'react-query';
+import { QUERY_USER_PROFILE_KEY } from '../../const/query.const';
+import { useUserProfileQuery } from '../../hooks/user/useUserProfileQuery';
 
 type NoteViewScreenProps = StackScreenProps<
   NoteSetScreenStackParamList,
@@ -14,9 +17,28 @@ export const NoteViewScreen: React.FC<NoteViewScreenProps> = ({
   navigation,
   route,
 }) => {
+  const queryClient = useQueryClient();
+  const profile = useUserProfileQuery();
+  const [hasInvalidated, setHasInvalidated] = useState(false);
+
+  /**
+   * Update user has_access status
+   */
+  useEffect(() => {
+    if (!hasInvalidated && profile.isSuccess && !profile.data.is_subscriber) {
+      queryClient.invalidateQueries(QUERY_USER_PROFILE_KEY);
+      setHasInvalidated(true);
+    }
+  }, [profile.isSuccess, hasInvalidated, profile.data]);
+
   return (
     <Container>
-      <NoteView noteId={route.params.noteId} />
+      {profile.isSuccess && profile.data.has_access && (
+        <NoteView noteId={route.params.noteId} />
+      )}
+      {profile.isSuccess && !profile.data.has_access && (
+        <Text>U has no access here</Text>
+      )}
     </Container>
   );
 };
